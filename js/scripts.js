e28c9f3cd569f7ac9c3e4ab50dde0b6b6adbe0c6
Samantha Callie <Hoomee90@gmail.com>
2023-09-12 13:38:17 -0700

    add root dictionary with finals to function

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,26 @@
 
 function rootFinder(num) {
   const rootValues = {
-
+    2 : "binary",
+    3 : "trinary",
+    4 : "quaternary",
+    5 : "quinary",
+    6 : "sextimal",
+    7 : "septimal",
+    8 : "octal",
+    9 : "nonary",
+    10 : "decimal",
+    11 : "elevenary",
+    12 : "dozenal",
+    13 : "baker's dozenal",
+    16 : "hex",
+    17 : "suboptimal",
+    20 : "icosi",
+    40 : "niftimal",
+    100 : "centesimal",
   }
   if (rootValues[num]) {
-
+    return rootValues[num];
   }
   return null
 }
