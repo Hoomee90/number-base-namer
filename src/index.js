198c90e69e51da15d73469e0a9ab4857ce9f8319
Samantha Callie <Hoomee90@gmail.com>
2023-11-09 14:12:11 -0800

    update project create developer environment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Utility Logic
 function flagHandler(...arrays) {
   //look for and process prime flags for each array input individually
