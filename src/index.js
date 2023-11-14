086b8824eeb0b1a4da68f5cd3dc3347dd1efb6c7
Samantha Callie <Hoomee90@gmail.com>
2023-11-14 15:48:23 -0800

    refactor code

--- src/index.js
+++ src/index.js
@@ -2,20 +2,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NumberNamer from './NumberNamer';
 
+const numberNamer = new NumberNamer();
+
 function inputHandler(e) {
   const output = document.querySelector("#numberResult");
   const input = e.target.value
   const validInput = new RegExp(/^-?([1-9]\d*|0)(\/-?([1-9]\d*|0))?$/);
   if (validInput.test(input)) {
     //checks to keep input within computational ability
-    if (input.length >=  16 && !input.includes("/")) {
+    if (input.length >= 15 && !input.includes("/")) {
+      output.innerHTML = "Too large! (Do you 𝘸𝘢𝘯𝘵 the page to crash?)"
+    } 
+    else if ((input.slice(0, input.indexOf("/")).length >= 14) || (input.slice(input.indexOf("/")).length >= 14)) {
       output.innerHTML = "Too large! (Do you 𝘸𝘢𝘯𝘵 the page to crash?)"
     }
-    else if ((input.slice(0, input.indexOf("/")).length > 14) || (input.slice(input.indexOf("/")).length > 15)) {
-      output.innerHTML = "Too large! (Do you 𝘸𝘢𝘯𝘵 the page to crash?)"
-    } else {
-      const namNumObject = new NumberNamer(input)
-      output.innerHTML = namNumObject.nameNum();
+    else {
+      console.log(numberNamer.showMemo())
+      output.innerHTML = numberNamer.nameNum(input);
     }
   }
   else {
